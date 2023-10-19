cf5e1d5366f4f2db3c77ddc85ba8f316429f9876
tquaddl <modellersamp@gmail.com>
2023-10-19 14:39:19 +0300

    adding TextEditingControllers and _showForm method

--- lib/main.dart
+++ lib/main.dart
@@ -35,11 +35,66 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _quantityController = TextEditingController();
+
+  void _showForm(BuildContext ctx, int? itemKey) async {
+    showModalBottomSheet(
+      context: ctx,
+      elevation: 5,
+      isScrollControlled: true,
+      builder: (_) => Container(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(ctx).viewInsets.bottom,
+          top: 15,
+          left: 15,
+          right: 15,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(hintText: 'Name'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _quantityController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(hintText: 'Quantity'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                _nameController.text = '';
+                _quantityController.text = '';
+                Navigator.of(context).pop();
+              },
+              child: Text('Create New'),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hive'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showForm(context, null),
+        child: const Icon(Icons.add),
       ),
     );
   }
